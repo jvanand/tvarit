be833e53af84e94b07eb84fb639c16076d2a9efe
jvanand <94153554+jvanand@users.noreply.github.com>
2021-11-12 08:35:56 +0530

    updated code with testcases

--- task1/solve.py
+++ task1/solve.py
@@ -4,17 +4,18 @@ def solve(A):
         if a>=13 and a<=19:
             if a!=15 and a!=16:
                 continue
-
         sum+=a
-
+    
     return sum
-
+    
 if __name__ == "__main__":
     n = 3
     A = []
     # iterating till the range
-    for i in range(n):
-        num = int(input())
-        A.append(num)
-
-    print(solve(A))
+    try:
+        for i in range(n):
+            num = int(input())
+            A.append(num)
+        print(solve(A))
+    except:
+        print("All inputs must be numeric")
